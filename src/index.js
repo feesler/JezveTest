03d39db69c7c5c274f6c7ccf39295bf195636941
Ilya Makarov <feesler@gmail.com>
2022-06-13 02:16:30 +0300

    + Export methods of current test environment from index of package

--- src/index.js
+++ src/index.js
@@ -35,3 +35,69 @@ export async function test(descr, action) {
         throw extError;
     }
 }
+
+export const baseUrl = () => testEnv.baseUrl();
+
+export const url = () => testEnv.url();
+
+export const isFullScenario = () => testEnv.isFullScenario();
+
+export const navigation = (action) => testEnv.navigation(action);
+
+export const setErrorHandler = () => testEnv.setErrorHandler();
+
+export const goTo = (link) => testEnv.goTo(link);
+
+export const parentNode = (elem) => testEnv.parentNode(elem);
+
+export const query = (...args) => testEnv.query(...args);
+
+export const queryAll = (...args) => testEnv.queryAll(...args);
+
+export const closest = (elem, selector) => testEnv.closest(elem, selector);
+
+export const hasClass = (elem, className) => testEnv.hasClass(elem, className);
+
+export const isVisible = (elem, recursive) => testEnv.isVisible(elem, recursive);
+
+export const select = (elem, value, bool) => testEnv.select(elem, value, bool);
+
+export const check = (elem) => testEnv.check(elem);
+
+export const onChange = (elem) => testEnv.onChange(elem);
+
+export const onBlur = (elem) => testEnv.onBlur(elem);
+
+export const prop = (elem, property) => testEnv.prop(elem, property);
+
+export const attr = (elem, attribute) => testEnv.attr(elem, attribute);
+
+export const waitForSelector = (selector, options) => (
+    testEnv.waitForSelector(selector, options)
+);
+
+export const waitForFunction = (condition, options) => (
+    testEnv.waitForFunction(condition, options)
+);
+
+export const wait = (condition, options) => testEnv.wait(condition, options);
+
+export const timeout = (ms) => testEnv.timeout(ms);
+
+export const global = (property) => testEnv.global(property);
+
+export const click = (elem) => testEnv.click(elem);
+
+export const input = (elem, val) => testEnv.input(elem, val);
+
+export const httpReq = (method, link, data, headers) => (
+    testEnv.httpReq(method, link, data, headers)
+);
+
+export const addResult = (descr, res) => testEnv.addResult(descr, res);
+
+export const setBlock = (title, category) => testEnv.setBlock(title, category);
+
+export const setDuration = (duration) => testEnv.setDuration(duration);
+
+export const getContent = () => testEnv.getContent();
