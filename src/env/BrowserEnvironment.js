d6d2f28caa5c1ffd62df793d07bed37bce4701ee
Ilya Makarov <feesler@gmail.com>
2022-09-16 16:28:22 +0300

    * Remove BrowserEnvironment.applyPolyfills() method

--- src/env/BrowserEnvironment.js
+++ src/env/BrowserEnvironment.js
@@ -383,87 +383,6 @@ export class BrowserEnvironment extends Environment {
         return this.vdoc.documentElement.innerHTML;
     }
 
-    /* eslint-disable */
-    scopedQuerySelectorPolyfill(view) {
-        try {
-            // test for scope support
-            view.document.querySelector(':scope *');
-        } catch (error) {
-            (function (ElementPrototype) {
-                // scope regex
-                var scope = /:scope(?![\w-])/gi;
-
-                // polyfill Element#querySelector
-                var querySelectorWithScope = polyfill(ElementPrototype.querySelector);
-
-                ElementPrototype.querySelector = function querySelector(selectors) {
-                    return querySelectorWithScope.apply(this, arguments);
-                };
-
-                // polyfill Element#querySelectorAll
-                var querySelectorAllWithScope = polyfill(ElementPrototype.querySelectorAll);
-
-                ElementPrototype.querySelectorAll = function querySelectorAll(selectors) {
-                    return querySelectorAllWithScope.apply(this, arguments);
-                };
-
-                // polyfill Element#matches
-                if (ElementPrototype.matches) {
-                    var matchesWithScope = polyfill(ElementPrototype.matches);
-
-                    ElementPrototype.matches = function matches(selectors) {
-                        return matchesWithScope.apply(this, arguments);
-                    };
-                }
-
-                // polyfill Element#closest
-                if (ElementPrototype.closest) {
-                    var closestWithScope = polyfill(ElementPrototype.closest);
-
-                    ElementPrototype.closest = function closest(selectors) {
-                        return closestWithScope.apply(this, arguments);
-                    };
-                }
-
-                function polyfill(qsa) {
-                    return function (selectors) {
-                        // whether the selectors contain :scope
-                        var hasScope = selectors && scope.test(selectors);
-
-                        if (hasScope) {
-                            // fallback attribute
-                            var attr = 'q' + Math.floor(Math.random() * 9000000) + 1000000;
-
-                            // replace :scope with the fallback attribute
-                            arguments[0] = selectors.replace(scope, '[' + attr + ']');
-
-                            // add the fallback attribute
-                            this.setAttribute(attr, '');
-
-                            // results of the qsa
-                            var elementOrNodeList = qsa.apply(this, arguments);
-
-                            // remove the fallback attribute
-                            this.removeAttribute(attr);
-
-                            // return the results of the qsa
-                            return elementOrNodeList;
-                        }
-
-                        // return the results of the qsa
-                        return qsa.apply(this, arguments);
-                    };
-                }
-            })(view.Element.prototype);
-        }
-    }
-    /* eslint-enable */
-
-    /** Apply polyfills not required by application, but needed for test engine */
-    applyPolyfills(view) {
-        this.scopedQuerySelectorPolyfill(view);
-    }
-
     setErrorHandler() {
         window.addEventListener('message', (e) => {
             this.viewError = copyObject(e.data);
@@ -489,8 +408,6 @@ export class BrowserEnvironment extends Environment {
                     if (!this.vdoc) {
                         throw new Error('View document not found');
                     }
-
-                    this.applyPolyfills(this.viewframe.contentWindow);
 
                     await this.onNavigate();
 
